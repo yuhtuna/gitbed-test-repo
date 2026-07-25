8b6744c42de6e42fbee0a915fb32245f4079e558
Tuan Nguyen <122138983+yuhtuna@users.noreply.github.com>
2026-07-25 16:02:40 -0400

    chore(devops): update pin_config.h hardware pin assignments

--- pin_config.h
+++ pin_config.h
@@ -1,8 +1,8 @@
 #ifndef PIN_CONFIG_H
 #define PIN_CONFIG_H
 
-// Hardware Pin Configurations - PCB Rev A
-#define STATUS_LED_PIN PB6
+// Hardware Pin Configurations - PCB Rev B
+#define STATUS_LED_PIN PB7
 #define UART_TX_PIN PA9
 #define UART_RX_PIN PA10
 
