5369a01293e83f462de7cf1c33ea5f2b4262ec34
Tuan Nguyen <122138983+yuhtuna@users.noreply.github.com>
2026-07-25 16:02:18 -0400

    initial commit: add pin_config.h

---
+++ pin_config.h
@@ -0,0 +1,11 @@
+#ifndef PIN_CONFIG_H
+#define PIN_CONFIG_H
+
+// Hardware Pin Configurations - PCB Rev A
+#define STATUS_LED_PIN PB6
+#define UART_TX_PIN PA9
+#define UART_RX_PIN PA10
+
+void init_pins();
+
+#endif // PIN_CONFIG_H
