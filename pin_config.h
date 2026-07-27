29e7fc50ee357b62169aa05447aa21aaf58402ba
Tuan Nguyen <122138983+yuhtuna@users.noreply.github.com>
2026-07-26 23:57:01 -0400

    chore(devops): update pin_config.h for INA_SCL pin reassignment

--- pin_config.h
+++ pin_config.h
@@ -2,9 +2,9 @@
 #define PIN_CONFIG_H
 
 // Kame_PDB Hardware Configurations
-#define VCC3SW_PIN P1
+#define VCC3SW_PIN P12
 #define ESC_EN_PIN P12
 #define INA_SDA_PIN P4
-#define INA_SCL_PIN P5
+#define INA_SCL_PIN P1
 
 #endif // PIN_CONFIG_H
