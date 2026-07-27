b35ca10d687b7b0e98ea2fc9786c7b49b067bb29
Tuan Nguyen <122138983+yuhtuna@users.noreply.github.com>
2026-07-26 23:50:26 -0400

    chore(devops): update src/gpio_driver.cpp for INA_SCL pin reassignment

--- src/gpio_driver.cpp
+++ src/gpio_driver.cpp
@@ -1,9 +1,9 @@
 // Auto-generated GPIO Driver Initialization by GitBed
 #include "pin_config.h"
 
-void init_gpio_esc_en() {
+void init_gpio_ina_scl() {
     GPIO_InitTypeDef gpio_init = {0};
-    gpio_init.Pin = ESC_EN_PIN;
+    gpio_init.Pin = INA_SCL_PIN;
     gpio_init.Mode = GPIO_MODE_OUTPUT_PP;
     gpio_init.Pull = GPIO_NOPULL;
     gpio_init.Speed = GPIO_SPEED_FREQ_HIGH;
