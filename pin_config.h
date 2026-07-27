ae73e4015d53595b82e978e6234e65870d5583cc
Tuan Nguyen <122138983+yuhtuna@users.noreply.github.com>
2026-07-27 00:11:59 -0400

    chore(devops): update pin_config.h for ESC_EN pin reassignment

--- pin_config.h
+++ pin_config.h
@@ -3,8 +3,8 @@
 
 // Kame_PDB Hardware Configurations
 #define VCC3SW_PIN P1
-#define ESC_EN_PIN P12
+#define ESC_EN_PIN P5
 #define INA_SDA_PIN P6
-#define INA_SCL_PIN P5
+#define INA_SCL_PIN P7 // Updated to avoid conflict with ESC_EN_PIN
 
 #endif // PIN_CONFIG_H
