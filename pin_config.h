e7a070f492e7baf9e570cb9553f1b264ca449fe6
Tuan <122138983+yuhtuna@users.noreply.github.com>
2026-07-27 22:20:03 -0400

    Initial commit: baseline firmware configuration

---
+++ pin_config.h
@@ -0,0 +1,10 @@
+#ifndef PIN_CONFIG_H
+#define PIN_CONFIG_H
+
+// Kame_PDB Hardware Configurations
+#define VCC3SW_PIN P1
+#define ESC_EN_PIN P12
+#define INA_SDA_PIN P4
+#define INA_SCL_PIN P5
+
+#endif // PIN_CONFIG_H
